7da9043f0db78697b19ed03833e7e8b3a5d61430
Jéssica Florentino <jdgflorentino@gmail.com>
2022-11-23 15:58:49 -0300

    feat: 2 - Criptografia de inversões (Testes)

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,15 @@
-# from challenges.challenge_encrypt_message import encrypt_message
+from challenges.challenge_encrypt_message import encrypt_message
+import pytest
 
 
 def test_encrypt_message():
-    pass
+    assert encrypt_message("AABBCC", 3) == "BAA_CCB"
+    assert encrypt_message("ABBCCA", 4) == "AC_CBBA"
+    assert encrypt_message("AABBCC", -1) == "CCBBAA"
+
+    with pytest.raises(TypeError):
+        encrypt_message(2, 5)
+    with pytest.raises(TypeError):
+        encrypt_message(None, None)
+    with pytest.raises(TypeError):
+        encrypt_message("AABBCC", "AA")
